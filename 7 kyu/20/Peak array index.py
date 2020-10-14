cb8df85db9faca88deaff9a86e93298e14fc77c9
FeiFu <m0627678@mail.fcu.edu.tw>
2020-10-14 22:38:59 +0800

    CodeWar_Test

--- 7 kyu/20/Peak array index.py
+++ 7 kyu/20/Peak array index.py
@@ -1,32 +1,52 @@
+"""
+    Given an array of ints, return the index such that the sum of the elements to the right of that index equals the sum of the elements to the left of that index.
+
+    If there is no such index, return -1. If there is more than one such index, return the left-most index.
+
+    For example:
+
+    peak([1,2,3,5,3,2,1]) = 3, because the sum of the elements at indexes 0,1,2 == sum of elements at indexes 4,5,6. We don't sum index 3.
+    peak([1,12,3,3,6,3,1]) = 2
+    peak([10,20,30,40]) = -1
+
+    Me Example:
+        [1,12,3,3,6,3,1]
+        Calculation Operating as follows:
+            [] [12, 3, 3, 6, 3, 1] 0 28
+            [1] [3, 3, 6, 3, 1] 1 16
+            [1, 12] [3, 6, 3, 1] 13 13
+            [1, 12, 3] [6, 3, 1] 16 10
+            [1, 12, 3, 3] [3, 1] 19 4
+            [1, 12, 3, 3, 6] [1] 25 1
+            [1, 12, 3, 3, 6, 3] [] 28 0
+
+    I think best solution:
+        def peak(arr):
+            for i, val in enumerate(arr):
+                if sum(arr[:i]) == sum(arr[i+1:]):
+                    return i
+            return -1
+
+    https://www.codewars.com/kata/5a61a846cadebf9738000076
+"""
+
+
 def peak(arr):
-    length = len(arr)
-    reverse = arr[::-1]
-    Count=0
-    ori_value, rev_value = 0, 0
-    reversearray =[]
-    original =[]
+    indexnumber = 0
+    for i in range(len(arr)):
 
-    for i in range(0, length//2):
-        reversearray.append(reverse[i])
-        original.append(arr[i])
-    reversearray = sorted(reversearray)
-    original =sorted(original)
-    for i in range(len(original)):
-        ori_value+=original[i]
-        rev_value+=reversearray[i]
-        if ori_value == rev_value:
-            Count+=1
-            print(ori_value, rev_value)
-
-    if Count == 0:
-        print("Nothing")
-        return -1
+        left = sum(arr[:i])
+        right = sum(arr[i + 1:])
+        print(arr[:i], arr[i + 1:], left, right)
+        if left == right:
+            # print(i)
+            indexnumber = i
+    if indexnumber > 0:
+        return indexnumber
     else:
-        print(Count)
-        return Count
+        return -1
+
 
 if __name__ == '__main__':
-
-    input = [9, 3, 2, 1, 2, 4, 3, 4, 5, 2, 1, 7, 8, 9, 1, 5, 7, 8, 5, 2, 2, 7, 1, 3, 7, 2, 6, 2, 2, 1, 1, 4, 2, 9, 2, 4, 6, 6, 5, 7, 4, 7, 5, 8, 5, 6, 6, 1, 1, 8, 5, 8, 8, 1, 9, 3, 2, 1, 2, 4, 3, 4, 5, 2, 1, 7, 8, 9, 1, 5, 7, 8, 5, 2, 2, 7, 1, 3, 7, 2, 6, 2, 2, 1, 1, 4, 2, 9, 2, 4, 6, 6, 5, 7, 4, 7, 5, 8, 5, 6, 6, 1, 1, 8, 5, 8, 8]
-
+    input = [1, 12, 3, 3, 6, 3, 1]
     peak(input)
